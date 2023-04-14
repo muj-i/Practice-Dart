ffccef509d302e54f8a8af049609889f40b04863
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-14 15:44:35 +0600

    Sample Post 4

--- Dart/variable.dart
+++ Dart/variable.dart
@@ -1,12 +1,20 @@
+  ///0 - comments section
+   // two forwardslash is for commenting or explain something or skipping any line in code & this only works in one line
+   // (/* - */) this method is for skipping a bunch of lines. EXAMPLE -
+    /*
+   print('Hello World');
+  print("Hello Dart");
+  print('''Hello Flutter''');
+    */
 //Starting with the function is responsible for all kinds of execution.
-main() {
+  main() {
   ///1 - Print Hello World
 
   print('Hello World'); //using one quotation mark
   print("Hello Dart"); //using two quotation mark
   print('''Hello Flutter'''); //using three quotation mark
 
-  ///2 -Line breaking, quotation mark & use of apostrophe
+  ///2 - Line breaking, quotation mark & use of apostrophe
 
   print('Rahim\'s \nPen'); //using one quotation mark, backslash apostrophe (\') for apostrophe & backslash n (\n) for line break
   print("Karim's \nPencil"); //using two quotation mark which is acceptable to use BASIC APOSTROPHE & backslash n (\n) for line break
@@ -14,29 +22,39 @@ main() {
      "Mobile"'''); //using three quotation mark which is also acceptable to use BASIC APOSTROPHE & we can use the BASIC SPACE & RETURN key to perform space & line break
 
   ///2 - Variable & Arithmetic Operators
-  /*
-  var x = 10;
-  var yo = 5;
-  yo = 140;
-  var z = x+yo;
-  var p = x*z;
-  var q = p/20;
-  var r = 20%3;
 
-  print(x);
-  print(yo);
-  print(z);
-  print(p);
-  print(q);
-  print(r);
+  var x = 20; //holding 20 in the variable name x
+    print(x); //printing the variable x
 
-  print(x+yo);
-  print(x-yo);
-  print(x*yo);
-  print(x/yo);
-  print(x%yo);
+  var y = 5; //holding 5 in the variable name y
+    print(y); //printing the variable y
 
-   */
+  y = 10; //update 10 in the variable name y
+    print(y); //printing the variable y after updating the value
+
+  var z = x+y; //holding the sum of var x & var y in the variable name z
+    print(z); //printing the variable z
+
+  var p = x*z; //holding the multiplication of var x & var z in the variable name p
+    print(p); //printing the variable p
+
+  var q = p/20; //holding the division of var p & 20 in the variable name q
+    print(q); //printing the variable q
+
+  var r = 20%3; //holding the modulus of 20 & 3 in the variable name r
+    print(r); //printing the variable r
+
+
+
+
+
+
+  print(x+y);
+  print(x-y);
+  print(x*y);
+  print(x/y);
+  print(x%y);
+
 
 
 
