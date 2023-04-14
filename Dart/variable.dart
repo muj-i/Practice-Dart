37c1e8be27b1723864522b3de7db7ee8e5d68dd6
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-14 14:47:57 +0600

    Sample Post 2

--- Dart/variable.dart
+++ Dart/variable.dart
@@ -1,4 +1,18 @@
-void main(){
-  print('m');
+//Starting with the function is responsible for all kinds of execution.
+main() {
+  //printing hello world
+  print('Hello World'); //using one quotation mark
+  print("Hello Dart"); //using two quotation
+  print('''Hello Flutter'''); //using 3
+
+  //line braking in one quotation and use of apostropie
+
+  print('Rahim\'s \nPen');
+  print("Karim's \nPencil");
+  print('''Rumi's
+     "Mobile"''');
+
+  //
+
 
 }
