bbf8ad1a6825aeee88aa605e1d28ed45ea4684b9
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-15 19:20:58 +0600

    The program prints all the listed numbers as we are told.

---
+++ Dart/Assignments/mod1assignment1.dart
@@ -0,0 +1,43 @@
+//Starting with the main function which is responsible for all kinds of execution.
+
+main() {
+
+  // List is defined as String.
+
+  List<String> phoneNumbers = [
+    "+88",
+    "01768131685",
+    "01768171985",
+    "01768111286",
+    "01768131685"
+  ];
+
+  //holding the first list item "+88" in the String named countryCode
+
+  String countryCode = phoneNumbers[0];
+
+  //listing all the number with the country code and holding them in a String named numberWithCode
+  //at first the String named countryCode
+  //then a line break
+  //then add the numbers by selecting each (ex:phoneNumbers[1]) from the phoneNumbers list
+
+  List<String> numberWithCode= [
+    countryCode +""+ phoneNumbers[1],
+    countryCode +""+ phoneNumbers[2],
+    countryCode +""+ phoneNumbers[3],
+    countryCode +""+ phoneNumbers[4],
+  ];
+
+  //for printing each number in a separate line
+  //holding the String numberWithCode in the String named numberWithCodeLineBreak
+  //then added line breaks using the .join
+
+  String numberWithCodeLineBreak = numberWithCode.join('\n');
+
+  //printing the full number with a title named Full Numbers
+  //then a line break
+  //then the String named numberWithCodeLineBreak
+
+  print("Full Numbers" +'\n'+ numberWithCodeLineBreak);
+
+}
