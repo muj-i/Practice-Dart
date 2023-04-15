9faf0923d1e9c441b7199b0d6644c9f3442dd48e
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-15 15:40:05 +0600

    Assignment Sample

---
+++ Assignment/mod1assignment1.dart
@@ -0,0 +1,10 @@
+main() {
+  List<String> userPhoneNum = [
+    "+88",
+    "01768131685",
+    "01768171985",
+    "01768111286",
+    "01768131685"
+  ];
+  print(userPhoneNum[0] +""+ userPhoneNum[1]);
+}
