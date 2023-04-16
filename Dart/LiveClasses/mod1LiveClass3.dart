ad1dcdae0bc94d26f7b598e2c0db42730534b051
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-16 22:36:26 +0600

    Operators

---
+++ Dart/LiveClasses/mod1LiveClass3.dart
@@ -0,0 +1,38 @@
+main(){
+/*
+int a = 40;
+int b = 2;
+int addition = a + b;
+int subtract = a - b;
+double division = a / b;
+
+
+ */
+
+
+  ///Dart Unary Operators
+
+  var x = 50;
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+
+
+  var xy = 50;
+  print(++xy);
+  print(++xy);
+  print(++xy);
+  print(++xy);
+  print(++xy);
+
+
+  var y = 300;
+  print(y++);
+  print(y++);
+  print(++y);
+  print(--y);
+  print(y--);
+
+}
