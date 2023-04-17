91b75c1490f844206c9a0eb7d668e3fc227b3c56
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-17 15:41:22 +0600

    ////////////////////////////

--- Dart/LiveClasses/Module1/mod1LiveClass3.dart
+++ Dart/LiveClasses/Module1/mod1LiveClass3.dart
@@ -43,7 +43,34 @@ main() {
   value *= 4;
   value /= 2;
   print(value);
+  print("");
+////////////////////////////////////
+  int x = 5;
+  int y = 7;
+  int z = 1;
 
+  print(x == y); // Equal to
+  print(x > y); // greater than
+  print(x < y); // less than
+  print(x >= z); // greater than or equal to
+  print('');
+  print(x <= z); // less than or equal to
+  print(x < y && x < z); // Logical and
+  print(x < y || x < z); // logical or
+
+  const String passportNo = '4545Txxxx3e434';
+  final int age;
+  age = 76;
+  print(passportNo);
+
+  /// Type casting
+  String myAge = '67';
+  int j = int.parse(myAge);
+  j += 1;
+  print(j);
+  myAge = j.toString();
+  double u = double.parse(myAge);
+  print(u);
 
 
 }
