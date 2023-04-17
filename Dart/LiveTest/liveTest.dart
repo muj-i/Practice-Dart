53c7045e2d20ecdfebe2205271c44265d1b050ae
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-18 00:05:12 +0600

    LiveTest1

--- Dart/LiveTest/liveTest.dart
+++ Dart/LiveTest/liveTest.dart
@@ -1,8 +1,13 @@
 void main (){
 
+    Map<String, dynamic> employee = {
+      'Id': 1234,
+      'Name': 'Duke John',
+      'Age': 34,
+      'Salary': 44000,
+      'Gender': 'Male'
+    };
 
-  
+    print(employee['Salary']);
 
-
-
-}
+  }
