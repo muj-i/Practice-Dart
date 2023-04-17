da918d5e56da112a01fe7c68b03f47609c9a697c
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-17 15:29:29 +0600

    Practice

--- Dart/experiments.dart
+++ Dart/experiments.dart
@@ -1,4 +1,4 @@
-void main(){
+void main() {
 
 /*
 ///4 - Dart List & List Properties
@@ -91,48 +91,5 @@ num.removeAt(2);
 
 print(num);*/
 //Starting with the main function which is responsible for all kinds of execution.
-
-
-
-    // List is defined as String.
-
-    List<String> phoneNumbers = [
-      "+88",
-      "01768131685",
-      "01768171985",
-      "01768111286",
-      "01768131685"
-    ];
-
-    //holding the first list item "+88" in a String named "countryCode"
-
-    String countryCode = phoneNumbers[0];
-
-    //listing all the number with the country code and holding them in a String named "numberWithCode"
-    //first write the string called "countryCode"
-    //then add the numbers by selecting each (ex:phoneNumbers[1]) from the "phoneNumbers" list
-
-    List<String> numberWithCode= [
-      countryCode + phoneNumbers[1],
-      countryCode + phoneNumbers[2],
-      countryCode + phoneNumbers[3],
-      countryCode + phoneNumbers[4],
-    ];
-
-
-
-    //for printing each number in a separate line
-    //holding the String "numberWithCode" in the String named "numberWithCodeLineBreak"
-    //then added line breaks using the .join('\n)
-
-    String numberWithCodeLineBreak = numberWithCode.join('\n');
-
-    //printing the full number with a title named Full Numbers
-    //then a line break using '\n'
-    //then the String named "numberWithCodeLineBreak"
-
-    print("Full Numbers" +'\n'+ numberWithCodeLineBreak);
-
-
 
 }
