2237e129420b915e6697b78d7406103f68de043d
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-17 15:28:53 +0600

    half

--- Dart/LiveClasses/Module1/mod1LiveClass3.dart
+++ Dart/LiveClasses/Module1/mod1LiveClass3.dart
@@ -1,44 +1,49 @@
-main(){
-/*
-int a = 40;
-int b = 2;
-int addition = a + b;
-int subtract = a - b;
-double division = a / b;
+main() {
+  int a = 41;
+  int b = 2;
+  int addition = a + b;
+  int subtract = a - b;
+  double division = a / b;
+  int multiple = a * b;
+  int modulus = a % b;
 
 
- */
+  print(addition);
+  print(subtract);
+  print(division);
+  print(multiple);
+  print(modulus);
+  print("");
 
 
   ///Dart Unary Operators
 
-  /*var x = 50;
-  print(x++);
-  print(x++);
-  print(x++);
-  print(x++);
-  print(x++);
+  double value = 2;
+  print(value);
+  print("");
+
+  ///all of them are same
+  //value = value + 1;
+  //print(value);
+
+  //value += 1;
+  //print(value);
+
+  //value++;
+  //print(value);
 
 
-  var xy = 50;
-  print(++xy);
-  print(++xy);
-  print(++xy);
-  print(++xy);
-  print(++xy);
+  ///print(value++); //eta age value print kore then add kore rakhe kintu print kore na, print kore porer line e
+  ///print(++value); //eta age value er sathe add kore then print kore
+  ///print(value--); //eta age value print kore then subtract kore rakhe kintu print kore na, print kore porer line e
+  ///print(--value); //eta age value er sathe subtract kore then print kore
+
+  value += 3;
+  value -= 2;
+  value *= 4;
+  value /= 2;
+  print(value);
 
 
-   */
-
-
-  //print(--y);
-  int y = 300;
-   y++;
-   //y++;
-   print(y++);
-   //print(y);
-
- print(++y);
-  //int(y--);
 
 }
