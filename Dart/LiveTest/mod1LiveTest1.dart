5590a7755697f4a87112e4f24760888636f35daf
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-18 00:25:26 +0600

    mod1LiveTest1

--- Dart/LiveTest/mod1LiveTest1.dart
+++ Dart/LiveTest/mod1LiveTest1.dart
@@ -1,3 +1,7 @@
+///Create a map (key value pair) of an employee, the keys will be employee = [Id, Name, Age, Salary, Gender].
+/// In this map [Id, Age, Salary] only these three keys' values will be in number.
+/// At last print out the Salary from the map.
+
 void main (){
 
     Map<String, dynamic> employee = {
