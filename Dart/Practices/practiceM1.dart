9e4d6ee2f46470e2b526c95e432f24c97a1129a8
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-19 16:05:36 +0600

    pm1

---
+++ Dart/Practices/practiceM1.dart
@@ -0,0 +1,116 @@
+import 'dart:io';
+void main() {
+
+  print('suns');
+  dynamic sunset = stdin.readLineSync();
+  print('sunr');
+  dynamic sunrise = stdin.readLineSync();
+  var addition = sunset + sunrise;
+  print(addition);
+
+
+  // Prompt the user to enter some input
+  //print('Enter your name:');
+
+  // Read the user's input from the console
+  // var name = stdin.readLineSync();
+
+  // Print out a message using the user's input
+  // print('Hello, $name!');
+
+
+
+
+/*
+///4 - Dart List & List Properties
+
+
+
+  var bdDiv =
+  [
+    'Dhaka',
+    'Khulna',
+    'Barisal',
+    'Chittagong',
+    'Mymensingh',
+    'Rajshahi',
+    'Sylhet',
+    'Rangpur'
+  ];
+ var res = bdDiv.length;
+ // var res = bdDiv.last;
+  //var res = bdDiv.;
+
+
+
+  //print(bdDiv);
+ // print(bdDiv[1]);
+  //print('${bdDiv[1]} and ${bdDiv[2]}');
+  //print(res);
+
+///5 - Dart Map
+/*
+  var wCouCap =
+  {
+    'Bandladesh' : 'Dhaka',
+    'India' : 'Delhi',
+    'Japan' : 'Tokyo'
+  };
+
+  print(wCouCap);
+  print(wCouCap['Japan']);
+
+ */
+
+///6 -
+
+
+
+  ///7 - Dart Constant
+
+  final w = 50000;
+  const e = 1700;
+
+  print(w-e*e/w);
+
+
+
+///8 - Dart Fixed Length List & Growable List
+
+  var city=[
+    'Dhaka',
+    'Khulna',
+    'Barisal',
+  ];
+
+  print(city);
+
+  city.add('nepal');
+
+  print(city);
+
+  const cityy=[
+    'Dhaka',
+    'Khulna',
+    'Barisal',
+  ];
+
+  print(cityy);
+
+
+///9 - List insert
+
+var num=[1, 2, 3,];
+
+num[0]=600;
+
+num.insert(2,10);
+num.insertAll(2,[50000,520]);
+num.removeAt(2);
+
+
+
+print(num);*/
+//Starting with the main function which is responsible for all kinds of execution.
+
+}
