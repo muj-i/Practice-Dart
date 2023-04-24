b8bad3bcd5aed31c6ea8069e0d80ed8e83fa0aad
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-24 15:54:13 +0600

    helloworld

--- Dart/hello.dart
+++ Dart/hello.dart
@@ -1,7 +1,8 @@
 void main(){
 
-  print('Hello World');
-  print('Hello World');
-  print('Hello World'); 
+  print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
+  print('Hello, World!');
 
 }
