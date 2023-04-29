b4c0e124a3a582553f6cace9dda69e6517bbed39
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-04-29 20:48:12 +0600

    pm2

--- Dart/Practices/practiceM2.dart
+++ Dart/Practices/practiceM2.dart
@@ -1,3 +1,4 @@
+
 void main() {
 /*var set=<int> {52,894};
 set.add(5);
@@ -7,6 +8,7 @@ print(set.isEmpty);
 print(set.length);
 */
 
+/*
 Map person =new Map();
 person['con']='nd';
 person['c4on']='n5d';
@@ -15,6 +17,59 @@ person.remove('con');
 //print(person.keys);
   print(person);
   print('muj-i');
+
+
+var per = new Map();
+per['name'] = 'muj';
+per['age'] = '565';
+print(per.keys);
+print(per.length);
+print(per.isEmpty);
+
+
+Map per = {
+
+  'name' : 'fral'
+
+} ;
+
+per.addAll({'chop': 'cow', 'fog' : 'dog'});
+//per.clear();
+  per.remove('fog');
+print(per);
+*/
+///if , if else, if else if
+/*
+  var marks=32;
+  if(marks>=80){
+    print("A+");
+  }
+  /*
+  else {
+    print("Below A+");
+  }
+   */
+  else if(marks<80 && marks>=70) {
+    print("A");
+  }
+  else if(marks<70 && marks>=60){
+    print("A-");
+  }
+  else if(marks<60 && marks>=50){
+    print("B");
+  }
+  else if(marks<50 && marks>=40){
+    print("C");
+  }
+  else if(marks<40 && marks>=33){
+    print("D");
+  }
+  else if(marks<33 && marks>=0){
+    print("F");
+  }
+
+ */
+
 
 
 
