5a3542feeb3cc249745e9efe945ee654a0419901
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-05-02 23:10:36 +0600

    mod2LiveClass1

--- Dart/LiveClasses/Module2/mod2LiveClass1.dart
+++ Dart/LiveClasses/Module2/mod2LiveClass1.dart
@@ -1,6 +1,48 @@
 void main(){
 
-  print("mod2LClass1");
-  print("2 May,2023");
+  ///SET
+
+  Set<String> students = {
+    'John',
+    'Tom',
+    'Tim',
+    'Duke',
+    'John', //duplicate value cant be printed in set but list can do that
+  };
+  students.add('Miok');
+  students.remove('Tom');
+  students.addAll({'elements', 'hkjk'});//set
+  students.addAll(['elemlents', 'hkjk']); //list
+  print(students);
+  print(students.elementAt(4));
+
+  ///MAP
+
+  Map<String, String> user ={
+    'firstName' : 'Tim',
+    'midName' : 'Bim',
+    'lastName' : 'Shim',
+  };
+  print(user);
+  print(user['firstName']);
+///
+
+  Map<int, String> stus= {
+    1 : 'pius',
+    2 : 'liom',
+    3 : 'gukm'
+  };
+  print(stus);
+  print(stus[2]);
+
+///
+
+  Map<String, dynamic> pInfo = {
+    'name' : "lio",
+    'age' : 65,
+    'money' : 5476.5646
+  };
+  pInfo['car'] = "bmw";
+  print(pInfo);
 
 }
