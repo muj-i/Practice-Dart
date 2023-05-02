4ac1dd076c03321736f8836e4e2dbd519e1dcbea
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-05-02 22:47:54 +0600

    Assignment1

--- Dart/Assignments/mod1Assignment1.dart
+++ Dart/Assignments/mod1Assignment1.dart
@@ -2,11 +2,7 @@
 ///phoneNumber = [ +88, 01768131685, 01768171985, 01768111286, 01768131685]
 ///And write program that prints out the full number like ‘+8801768171985’ You have to print out all numbers given in the list except +88.
 
-//Starting with the main function which is responsible for all kinds of execution.
-
  void main() {
-
-  // List is defined as String.
 
   List<String> phoneNumbers = [
     "+88",
@@ -21,8 +17,6 @@
   String countryCode = phoneNumbers[0];
 
   //listing all the number with the country code and holding them in a String named "numberWithCode"
-  //first write the string called "countryCode"
-  //then add the numbers by selecting each (ex:phoneNumbers[1]) from the "phoneNumbers" list
 
   List<String> numberWithCode= [
     countryCode + phoneNumbers[1],
@@ -32,12 +26,8 @@
   ];
 
   //for printing each number in a separate line
-  //holding the String "numberWithCode" in the String named "numberWithCodeLineBreak"
-  //then added line breaks using the .join('\n')
 
   String numberWithCodeLineBreak = numberWithCode.join('\n');
-
-  //printing the String named "numberWithCodeLineBreak"
 
   print(numberWithCodeLineBreak);
 
