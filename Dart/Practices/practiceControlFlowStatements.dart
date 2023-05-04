5be4975ab10f88c828e73f8882f8fe601b4cfcf1
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-05-04 20:02:56 +0600

    practiceControlFlowStatements

--- Dart/Practices/practiceControlFlowStatements.dart
+++ Dart/Practices/practiceControlFlowStatements.dart
@@ -1,31 +1,32 @@
 void main(){
-  int marks = -2;
+  int marks = -101;
   
   if (marks >= 80 && marks <= 100) {
     print('A+');
   }
-  if (marks >= 70 && marks <= 79) {
+  else if (marks >= 70 && marks <= 79) {
     print('A');
   }
-  if (marks > 60 && marks < 69) {
+  else if (marks > 60 && marks < 69) {
     print('A-');
   }
-  if (marks > 50 && marks < 59) {
+  else if (marks > 50 && marks < 59) {
     print('B');
   }
-  if (marks > 40 && marks < 49) {
+  else if (marks > 40 && marks < 49) {
     print('C');
   }
-  if (marks > 40 && marks < 49) {
+  else if (marks > 40 && marks < 49) {
     print('D');
   }
-  if (marks >= 33 && marks <= 39) {
+  else if (marks >= 33 && marks <= 39) {
     print('E');
   }
-  if (marks >= 0 && marks <= 32) {
+  else if (marks >= 0 && marks <= 32) {
     print('Fail');
   }
-  if (marks > -1 && marks >= 101) {
+  else {
     print('Invalid Result');
   }
+
 }
