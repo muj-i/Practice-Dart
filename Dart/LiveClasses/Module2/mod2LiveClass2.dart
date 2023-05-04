3ed558d44829186bf44a099d0dac8d979a27a247
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-05-05 00:32:30 +0600

    m2l2

--- Dart/LiveClasses/Module2/mod2LiveClass2.dart
+++ Dart/LiveClasses/Module2/mod2LiveClass2.dart
@@ -1,4 +1,26 @@
-void main(){
+///KBPro,appdata,velvet2,
+void main() {
+  int day = 8;
+  switch (day) {
+    case 1:
+      print('Saturday');
+      break;
+    case 2:
+      print('Sunday');
+      break;
+    case 3:
+      print('Monday');
+      break;
+    case 4:
+      print('Tuesday');
+      break;
+    case 5:
+      print('Wednesday');
+  }
 
-  print('me');
+
+  ///loop
+  ///
+
+  ///
 }
