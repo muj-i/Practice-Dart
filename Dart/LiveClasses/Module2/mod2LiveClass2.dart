35fd7a9fc673273bc3bf37c1bcc45f74dedda652
Wisloo <99077742+Wisloo@users.noreply.github.com>
2023-05-05 16:11:34 +0600

    mod2LC2

--- Dart/LiveClasses/Module2/mod2LiveClass2.dart
+++ Dart/LiveClasses/Module2/mod2LiveClass2.dart
@@ -1,4 +1,4 @@
-///KBPro,appdata,velvet2,
+
 void main() {
   int day = 8;
   switch (day) {
@@ -16,11 +16,55 @@ void main() {
       break;
     case 5:
       print('Wednesday');
+      break;
+    case 6:
+      print('Thursday');
+      break;
+    case 7:
+    case 8:
+      print('Friday');
+      break;
+    default:
+      print('Wrong day!');
   }
 
-
+print(' ');
   ///loop
-  ///
+  // count koto theke shuru korte hobe
+  // koto porjonto program run korte hobe
+  // koto bar count barate hobe
 
-  ///
+  // starting(once) ; condition ; increment/decrement
+                                ///start = start + 1 //same
+  for (int start = 0; start <= 10; start++) {
+    // print Hungry!!! with allignment number
+    print('$start Hungry!!!');
+  }
+  print(' ');
+
+  /// String interpolation
+
+  String name = 'Tim';
+  int age = 65;
+  double salary = 726446.554;
+  String information = 'Name: $name, Age: $age, Salary: \$$salary'; // String interpolation
+  print(information);
+
+///
+  print(' ');
+
+  List<String> candidates = ['John', 'Pinch', 'Riyuk'];
+
+  for (int allignment = 0; allignment < candidates.length; allignment = allignment + 1)
+  {
+    print('Greetings ${candidates[allignment]} ');
+    print('Morning ' + candidates[allignment]);
+  }
+   print(' ');
+///for in loop
+
+  for (String candidatesName in candidates)
+  {
+   print('Evening' +' '+ candidatesName);
+  }
 }
