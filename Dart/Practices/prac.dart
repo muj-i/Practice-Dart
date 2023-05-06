5222906594bff7f69f485466e62e7d9bc65d0165
muj <muj.i@icloud.com>
2023-05-06 15:49:27 +0600

    p

--- Dart/Practices/prac.dart
+++ Dart/Practices/prac.dart
@@ -1,3 +1,4 @@
+
 void main(){
 
   String fName = 'Mujahedul';
@@ -5,10 +6,23 @@ void main(){
   print('Full name is $fName $lName');
 
 
+    Map <String, String> info = {
+      'nmae' : 'taw',
+      'age' : '652',
+      'address' : 'khulna',
+    };
+
+  for (String key in info.keys)
+  {
+    print('$key: ${info[key]}');
+  }
+
+List <String> ty = ['china', '155','14564'];
+  print(ty);
 
 
-
-
+Set <int> fuh = {3456156,15614561,16145611};
+print(fuh);
 
 
 
