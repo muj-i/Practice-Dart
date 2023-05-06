9a59b5417608cc7ed3037c1f52fbac9ca2d847be
muj <99077742+muj-i@users.noreply.github.com>
2023-05-07 01:19:56 +0600

    Module 2 Assignment 1

--- Dart/Assignments/mod2Assignment1.dart
+++ Dart/Assignments/mod2Assignment1.dart
@@ -10,10 +10,10 @@ studentGrade(String name , int score) {
   else if (score >= 80 && score <= 89) {
     return 'B';
   }
-  else if (score > 70 && score < 79) {
+  else if (score >= 70 && score <= 79) {
     return('C');
   }
-  else if (score > 60 && score < 69) {
+  else if (score >= 60 && score <= 69) {
     return('D');
   }
   else if (score >= 0 && score <= 59) {
@@ -26,7 +26,7 @@ studentGrade(String name , int score) {
 
 void main() {
   String studentName = "Mujahedul Islam";
-  int testScore = 66;
+  int testScore = 79;
 
   String grade = studentGrade(studentName, testScore);
 
