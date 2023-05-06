8b4bd318cc142c33c7421f61af8d7cbf423b1a7c
muj <99077742+muj-i@users.noreply.github.com>
2023-05-07 01:14:45 +0600

    ass

--- Dart/Assignments/mod2Assignment1.dart
+++ Dart/Assignments/mod2Assignment1.dart
@@ -1,5 +1,36 @@
-void main() {
-  
+///Here's a Dart function called studentGrade
+///that takes in the student's name and test score,
+///and returns their corresponding grade
+///based on the provided grading scale
 
+studentGrade(String name , int score)
+{
+  if (score >= 90 && score <= 100) {
+    return 'A';
+  }
+  else if (score >= 80 && score <= 89) {
+    return 'B';
+  }
+  else if (score > 70 && score < 79) {
+    return('C');
+  }
+  else if (score > 60 && score < 69) {
+    return('D');
+  }
+  else if (score >= 0 && score <= 59) {
+    return('F');
+  }
+  else {
+    return('Invalid Grade');
+  }
+}
+
+void main() {
+  String studentName = "Mujahedul Islam";
+  int testScore = 66;
+
+  String grade = studentGrade(studentName, testScore);
+
+  print("$studentName's grade: $grade");
 
 }
