26694fe6c6f3c650f7631ccfd575fc2e26fad527
muj <99077742+muj-i@users.noreply.github.com>
2023-05-08 22:30:02 +0600

    supClass6

---
+++ Dart/SupportClass/supClass6.dart
@@ -0,0 +1,18 @@
+List<int> getEvNums(List<int> nums){
+  List<int> evNums = [];
+
+  for (int x in nums){
+    if (x % 2 == 0) {
+      evNums.add(x);
+    }
+  }
+  return evNums;
+}
+
+void main(){
+  List<int> nums = [2,3,6,9,4];
+  List<int> evenNums = getEvNums(nums);
+
+  print(evenNums);
+
+}
