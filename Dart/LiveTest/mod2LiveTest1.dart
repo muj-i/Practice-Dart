d6b490081d8dc43e9569389fea02e75969e26607
muj <99077742+muj-i@users.noreply.github.com>
2023-05-08 23:17:13 +0600

    lt2

--- Dart/LiveTest/mod2LiveTest1.dart
+++ Dart/LiveTest/mod2LiveTest1.dart
@@ -1,33 +1,21 @@
-import 'dart:io';
+getOddNumbers(List<int> userInput)
+{
+  List<int> oddNumbersList = [];
 
-List<int> getOddNumbers(List<int> inputList) {
-  List<int> outputList = [];
-  for (int i = 0; i < inputList.length; i++) {
-    if (inputList[i] % 2 != 0) {
-      outputList.add(inputList[i]);
+  for (int singleInput in userInput){
+    if (singleInput % 2 != 0) {
+      oddNumbersList.add(singleInput);
+
     }
   }
-  return outputList;
+  return oddNumbersList;
 }
 
+void main()
+{
+  List<int> userInput = [2, 5, 8, 11, 13, 18, 21, 24];
+  List<int> oddNumbers = getOddNumbers(userInput);
 
+  print(oddNumbers);
 
-
-void main() {
-  // Take input from user
-  print("Enter a list of integers separated by spaces:");
-  List<String> input = stdin.readLineSync()!.split(' ');
-
-  // Convert input to list of integers
-  List<int> inputList = [];
-  for (int i = 0; i < input.length; i++) {
-    inputList.add(int.parse(input[i]));
-  }
-
-  // Get output list of odd numbers
-  List<int> outputList = getOddNumbers(inputList);
-
-  // Print input list and output list
-  print("Input list: $inputList");
-  print("Output list: $outputList");
 }
