2e776c9930c38f24794143db9fddc0dc77c21817
muj <99077742+muj-i@users.noreply.github.com>
2023-05-10 00:26:45 +0600

    oop

---
+++ Dart/Practices/OOP/dart-oop.dart
@@ -0,0 +1,19 @@
+import 'class.dart';
+
+main() {
+  // instance / object
+  Mobile iPhone6S = Mobile(); // object creation
+  iPhone6S.year = 2015; // property access & modify
+  iPhone6S.model = 'iPhone 6s Plus';
+  print(iPhone6S.year);
+  print(iPhone6S.model);
+
+
+  Mobile xiomi = Mobile();
+  xiomi.year = 2019;
+  xiomi.model = 'Xoimi X Plus';
+  print(xiomi.model);
+  print(xiomi.year);
+}
+
+
