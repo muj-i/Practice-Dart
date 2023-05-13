24e29c25fd2808df48bf413724bd9bb3bf2d7309
muj <99077742+muj-i@users.noreply.github.com>
2023-05-13 19:48:22 +0600

    loop

--- Dart/Practices/loopPrac.dart
+++ Dart/Practices/loopPrac.dart
@@ -1,7 +1,18 @@
-void main(){
 
-print ('jhhjfhhf');
-  
-666
+void main(){
+  for (int p = 0; p < 5; p++){
+
+    if ( p % 2 == 0){
+      print(p);
+
+    }
+
+  }
+
+List me = ['MI', 'NI', 'PI'];
+  for (String leea in me) {
+    print(leea);
+  };
+
 
 }
