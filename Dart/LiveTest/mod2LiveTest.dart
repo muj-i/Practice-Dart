062c6b7e3f8fe0f8f42d8228dc519e7a7863c9bf
muj <99077742+muj-i@users.noreply.github.com>
2023-05-16 13:24:17 +0600

    Module 2 Live Test

--- Dart/LiveTest/mod2LiveTest.dart
+++ Dart/LiveTest/mod2LiveTest.dart
@@ -1,3 +1,10 @@
+/*
+Write a Dart function called getOddNumbers that takes a list of integers as input and returns a new list containing only the odd numbers from the input list.
+Write a Dart program that uses this function to take input from the user, and prints both the input list and the output list using the print() function.
+For example, if the user inputs the list [2, 5, 8, 11, 13, 18, 21, 24], the program should return the output list [5, 11, 13, 21].
+Hint: You can use an if statement to check if a number is odd or even.
+ */
+
 getOddNumbers(List<int> userInput)
 {
   List<int> oddNumbersList = [];
